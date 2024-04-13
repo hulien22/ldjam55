45bea50d27434d396101450437ddfa7ebae9ad88
TElliott <timelliott2@gmail.com>
2024-04-13 12:51:11 -0400

    Update camera movement.  Working with wasd and edge of screen.

--- scripts/moveCamera.gd
+++ scripts/moveCamera.gd
@@ -1,18 +1,21 @@
 extends Area2D
 
-@export var speed = 400 # How fast the player will move (pixels/sec).
+var camera_speed_wasd = 500 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
-var spd = 10
-var radius_required_to_move = 100
+var screen
+var camera_speed_mouse = 500
+var radius_required_to_move = 75
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	screen_size = get_viewport_rect().size
+	screen = get_viewport_rect()
+	screen_size = screen.size
+	get_tree().get_root().size_changed.connect(_on_viewport_resize) 
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
+	#WASD control
+	var velocity = Vector2.ZERO 
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -23,14 +26,27 @@ func _process(delta):
 		velocity.y -= 1
 
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
+		velocity = velocity.normalized() * camera_speed_wasd
 
 	position += velocity * delta
 
 
+	#Mouse on edge of screen control
+	var mouse_position = get_viewport().get_mouse_position()
 
+	if screen.has_point(mouse_position) && mouse_position.x < radius_required_to_move:
+		position +=  Vector2.LEFT * camera_speed_mouse * delta
 
-	var mouse_position = get_global_mouse_position()
-	var mouse_delta = mouse_position - global_position
-	if (mouse_delta.length() >= radius_required_to_move):
-		position += (mouse_delta / radius_required_to_move) * spd * delta
+	if screen.has_point(mouse_position) && abs(mouse_position.x-screen_size.x) < radius_required_to_move:
+		position +=  Vector2.RIGHT * camera_speed_mouse * delta
+
+	if screen.has_point(mouse_position) && mouse_position.y < radius_required_to_move:
+		position +=  Vector2.UP * camera_speed_mouse * delta
+
+	if screen.has_point(mouse_position) && abs(mouse_position.y-screen_size.y) < radius_required_to_move && mouse_position.y < screen_size.y:
+		position +=  Vector2.DOWN * camera_speed_mouse * delta
+
+func _on_viewport_resize():
+	print("resize")
+	screen = get_viewport_rect()
+	screen_size = screen.size
