c536bf8df5a3619e1d0063a68e4b8d554c715ece
Eimh <41556634+Eimh@users.noreply.github.com>
2024-04-13 00:22:04 -0400

    nav

---
+++ scripts/movable.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var agent: NavigationAgent2D
+var first: bool = true
+@export var size: int = 500
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	agent = $NavigationAgent2D
+	agent.target_position = Vector2(randi()%size-size/2, randi()%size-size/2)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if first:
+		first = false
+		return
+	if not agent.is_target_reached() and not agent.is_navigation_finished():
+		var dir = global_position.direction_to(agent.get_next_path_position())
+		transform = transform.translated(dir)
+	else:
+		agent.target_position = Vector2(randi()%size-size/2, randi()%size-size/2)
+		print(agent.target_position)
