5f02ea2388238fdad0a4a3ede9daf40dfba04438
hulien22 <hejulien22@gmail.com>
2024-04-12 23:09:42 -0700

    clean up

--- scripts/NavAgentComponent.gd
+++ scripts/NavAgentComponent.gd
@@ -3,11 +3,13 @@ extends NavigationAgent2D
 
 @export var actor: Node2D
 @export var size: int = 500
+@export var speed: float = 100
 
 var first: bool = true
 
 func _ready():
 	target_position = Vector2(randi()%size-size/2, randi()%size-size/2)
+	speed = randf() * 200 + 50
 
 func _physics_process(delta):
 	if first:
@@ -15,7 +17,7 @@ func _physics_process(delta):
 		return
 	if not is_target_reached() and not is_navigation_finished():
 		var dir = actor.global_position.direction_to(get_next_path_position())
-		actor.transform = actor.transform.translated(dir)
+		actor.transform = actor.transform.translated(dir * speed * delta)
 	else:
 		target_position = Vector2(randi()%size-size/2, randi()%size-size/2)
-		print(target_position)
+		#print(target_position)
