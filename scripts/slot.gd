64d1465da2d06d546e1dc86b854a69aa5a955808
joseph dooley <josephldooley@gmail.com>
2024-04-14 01:08:10 -0400

    swap items in the menu bar

--- scripts/slot.gd
+++ scripts/slot.gd
@@ -6,21 +6,39 @@ func _ready():
 	pass # Replace with function body.
 
 func _can_drop_data(at_position, data):
-	return typeof(data) == TYPE_DICTIONARY and data.has("summon_stats") and get_child_count()==0
+	return typeof(data) == TYPE_DICTIONARY and data.has("summon_stats")
 
 func _drop_data(at_position, data):
 	var summon = SummonTexture.new()
 	summon.stats = data["summon_stats"]
 	summon.size = summon.stats.image.get_size()*2
 	var origin_node = data["origin_node"]
+	
+	#clear origin slot or swap summon
+	var old_summon = get_summon(self)
 	remove_summon(origin_node)
+	
+	if old_summon!=null:
+		var swap_summon = SummonTexture.new()
+		swap_summon.stats = old_summon.stats
+		swap_summon.size = old_summon.size
+		origin_node.add_child(swap_summon)
+	
+	#clear current slot and add new summon
+	remove_summon(self)
 	add_child(summon)
 	
 func remove_summon(parent_node):
 	for child_node in parent_node.get_children():
-		if parent_node is SlotTexture:
+		if parent_node is SlotTexture and child_node is SummonTexture:
 			parent_node.remove_child(child_node)
+			child_node.queue_free()	
 
+func get_summon(parent_node):
+	for child_node in parent_node.get_children():
+		if parent_node is SlotTexture and child_node is SummonTexture:
+			return child_node
+	return null	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
