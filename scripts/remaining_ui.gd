930e8bea70460e85667614509c71f395b7cc24d1
TElliott <timelliott2@gmail.com>
2024-04-14 12:18:15 -0400

    Updates to UI.

--- scripts/remaining_ui.gd
+++ scripts/remaining_ui.gd
@@ -2,10 +2,9 @@ class_name remaining_ui
 extends Control
 
 var count = 100
-var count_text: Label
+@export var count_text: Label
 
 func _ready():
-	count_text = $count_text
 	count_text.text = str(count)
 
 func on_death(x, y):
