fa26ed40122fe1a93f44e7c580663baa2e94e760
Eimh <41556634+Eimh@users.noreply.github.com>
2024-04-15 22:27:32 -0400

    fix maybe

--- scripts/remaining_ui.gd
+++ scripts/remaining_ui.gd
@@ -22,8 +22,6 @@ func _ready():
 func _process(delta):
 	count_gold.text = str(gold)
 
-	
-
 func on_death(x, y):
 	count -= 1
 	count_text.text = str(count)
@@ -32,7 +30,7 @@ func on_death(x, y):
 	#the player caused the death of this NPC
 	if x.player:
 		gold += 10
-	if count == 1 and not has_player_died: #player won
+	if count == 1 and !has_player_died: #player won
 		game_over_scene.on_champion_won()
 		
 
